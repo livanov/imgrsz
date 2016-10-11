d210fb920055e045dbb982174cc6faf5cdd00d93
Headnet <headnet@cphca.dk>
2016-10-11 14:41:07 +0300

    Image compressor initial commit

---
+++ ImageCompressor.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ImageCompressor_v1
+{
+    class ImageCompressor
+    {
+        private static readonly List<byte[]> ImageTypes = new List<byte[]>
+        {
+            new byte[] {0xFF, 0xD8}, //jpg
+            new byte[] {0x42, 0x4D}, //bmp
+            new byte[] {0x47, 0x49, 0x46}, //gif
+            new byte[] {0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A} //png
+        };
+
+        private readonly string _path;
+
+        public ImageCompressor(string path)
+        {
+
+            // if it is not a directory, neither a file
+            if (!Directory.Exists(path) && !File.Exists(path))
+            {
+                throw new ArgumentException(@"Not an image file or a directory.", path);
+            }
+
+            // if it is a file but not an image file
+            if (File.Exists(path) && !IsImageFile(path))
+            {
+                throw new ArgumentException(@"Not an image file or a directory.", path);
+            }
+
+            _path = path;
+
+        }
+
+        public void Compress(string destinationDir, bool recursive)
+        {
+            Compress(_path, destinationDir, recursive);
+        }
+
+        public void Compress(string sourcePath, string destinationDir, bool recursive)
+        {
+            if (!Directory.Exists(destinationDir))
+            {
+                throw new ArgumentException(@"Output directory does not exist.", destinationDir);
+            }
+
+            if (File.Exists(sourcePath))
+            {
+                ProcessImageFile(sourcePath, destinationDir);
+            }
+            else
+            {
+                foreach (var file in Directory.GetFiles(sourcePath))
+                {
+                    if (IsImageFile(file))
+                    {
+                        ProcessImageFile(file, destinationDir);
+                    }
+                }
+
+                if (!recursive) return;
+
+                Parallel.ForEach(Directory.GetDirectories(sourcePath), directory =>
+                //foreach (var directory in Directory.GetDirectories(sourcePath))
+                {
+                    var innerDestinationDir = Path.Combine(destinationDir, new DirectoryInfo(directory).Name);
+
+                    if (!Directory.Exists(innerDestinationDir))
+                    {
+                        Directory.CreateDirectory(innerDestinationDir);
+                    }
+
+                    Compress(directory, innerDestinationDir, recursive);
+                }
+                );
+            }
+        }
+
+        public static bool IsImageFile(string path)
+        {
+            var position = 0;
+            var possibilities = ImageTypes.Count;
+            using (var fs = new FileStream(path, FileMode.Open, FileAccess.Read))
+            {
+                var reversedMask = new bool[ImageTypes.Count];
+
+                int singleByte;
+                while ((singleByte = fs.ReadByte()) != -1)
+                {
+                    for (var i = 0; i < ImageTypes.Count; i++)
+                    {
+                        if (reversedMask[i] || ImageTypes[i][position] == singleByte) continue;
+
+                        reversedMask[i] = true;
+                        if (--possibilities == 0)
+                        {
+                            return false;
+                        }
+                    }
+
+                    position++;
+                    for (var i = 0; i < ImageTypes.Count; i++)
+                    {
+                        // there is a possible byte start
+                        // and
+                        // byte start is exhausted (position index is at the next possible position)
+                        if (!reversedMask[i] && position == ImageTypes[i].Length)
+                        {
+                            return true;
+                        }
+                    }
+                }
+
+                return false;
+            }
+        }
+
+        private void ProcessImageFile(string sourceFilePath, string destinationDir)
+        {
+            var destinationFilePath = Path.Combine(destinationDir, Path.GetFileName(sourceFilePath));
+            string tempFileName;
+
+            using (var bm = new Bitmap(sourceFilePath))
+            {
+                bm.SetPixel(0, 0, bm.GetPixel(0, 0));
+
+
+                if (sourceFilePath != destinationFilePath)
+                {
+                    bm.Save(destinationFilePath);
+                    return;
+                }
+
+                tempFileName = Path.GetTempFileName();
+                bm.Save(tempFileName);
+            }
+         
+            File.Delete(destinationFilePath);
+            File.Move(tempFileName, destinationFilePath);
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Threading;
+using MessageBox = System.Windows.MessageBox;
+
+namespace ImageCompressor_v1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static Thread RunningThread { get; private set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SourceButton_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new FolderBrowserDialog();
+            dialog.ShowDialog();
+            SourceBox.Text = dialog.SelectedPath;
+        }
+
+        private void DestinationButton_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new FolderBrowserDialog();
+            dialog.ShowDialog();
+            DestinationBox.Text = dialog.SelectedPath;
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            DestinationBox.Text = SourceBox.Text;
+            DestinationBox.IsEnabled = false;
+            DestinationButton.IsEnabled = false;
+        }
+
+        private void OverwriteBox_OnUnchecked(object sender, RoutedEventArgs e)
+        {
+            DestinationBox.IsEnabled = true;
+            DestinationButton.IsEnabled = true;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var srcDir = SourceBox.Text.Trim(' ', '"');
+            var destDir = DestinationBox.Text.Trim(' ', '"');
+
+            if (srcDir.Contains(destDir))
+            {
+                MessageBox.Show("Destination directory can't be within source directory");
+            }
+
+            if (OverwriteBox.IsChecked != null && !OverwriteBox.IsChecked.Value)
+            {
+                destDir = Path.Combine(destDir, new DirectoryInfo(srcDir).Name);
+            }
+
+            if (string.IsNullOrEmpty(srcDir) || string.IsNullOrEmpty(destDir))
+            {
+                MessageBox.Show("Missing field");
+                return;
+            }
+
+            if (!Directory.Exists(srcDir))
+            {
+                MessageBox.Show("Source directory does not exist.");
+            }
+
+            if (!Directory.Exists(destDir))
+            {
+                Directory.CreateDirectory(destDir);
+            }
+
+            if ((string)StartButton.Content == "Start")
+            {
+                StartButton.Content = "STOP";
+
+                RunningThread = new Thread(() =>
+                {
+                    var imgCompressor = new ImageCompressor(srcDir);
+                    imgCompressor.Compress(destDir, true);
+                    Dispatcher.BeginInvoke(DispatcherPriority.Background, new Action(() =>
+                    {
+                        StartButton.Content = "Start";
+                        MessageBox.Show("Done!", "Task completed", MessageBoxButton.OK);
+                    }));
+                });
+                RunningThread.Start();
+            }
+            else
+            {
+                StartButton.Content = "Start";
+
+                if (RunningThread != null && RunningThread.IsAlive)
+                {
+                    MessageBox.Show("Abort!", "Task aborted", MessageBoxButton.OK);
+                    RunningThread.Abort();
+                }
+            }
+
+        }
+    }
+}
